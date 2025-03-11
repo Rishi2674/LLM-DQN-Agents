847f54be3e65a928c1ca9fe5fdafb79626f3565a
Rishi2674 <rupadhyay2674@gmail.com>
2025-03-11 10:41:53 +0530

    model chamged to mistral 7B and improved prompts

--- llm.py
+++ llm.py
@@ -5,8 +5,7 @@ import os
 load_dotenv()
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')  # Get the token from your .env file
-model_name = "meta-llama/Llama-2-7b-hf"
-
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
 # Print the access token to ensure it's being passed correctly
 print(f"Using access token: {ACCESS_TOKEN}")
 
@@ -24,9 +23,12 @@ except Exception as e:
     print(f"Error loading model: {e}")
 
 # Generate text
-input_text = "What is the capital of France?"
-inputs = tokenizer(input_text, return_tensors="pt").to("cuda")  # Move to GPU if available
-output = model.generate(**inputs, max_length=50)
+prompt = """
+    What is the capital Of indiB?
+    Strictly answer in one word
+"""
+inputs = tokenizer(prompt, return_tensors="pt")  # Move to GPU if available
+output = model.generate(**inputs, max_new_tokens = 50, temperature=0.1, top_p=0.9, repetition_penalty=1.2)
 
 # Print response
 print(tokenizer.decode(output[0], skip_special_tokens=True))
