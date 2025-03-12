d75b4a3f37d70ccf815890ce71673c88b13b2c55
Rishi2674 <rupadhyay2674@gmail.com>
2025-03-12 12:51:47 +0530

    pushing necessary changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from envs.maze_env import MazeEnvironment
 from models.dqn_model import DQNModel
 from train_model import run_training_loop
 from models.dqn_llm_model import DQN_LLM_Model
+from models.dqn_model import DQNModel
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -22,4 +23,4 @@ if __name__ == "__main__":
     # dqn_model = DQNModel(state_size=100, action_size=5, maze=maze_env, device=device)
     dqn_model = DQN_LLM_Model(state_size=100,action_size=5,maze=maze_env,device=device )
     print("DQN Model initialized and ready for training/testing.")
-    run_training_loop(dqn_model, maze_env, num_episodes=3000, max_steps=100, batch_size=32, target_update_freq=10)
+    run_training_loop(dqn_model, maze_env, num_episodes=1500, max_steps=100, batch_size=32, target_update_freq=10)
